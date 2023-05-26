feb44f644f2fcab54f1ab6bea4416be5a9ba795d
Tomoki Sato <nck.cd.339@gmail.com>
2023-05-26 09:25:03 +0900

    Add endpoint for testing AppConfig sidecar

--- main.go
+++ main.go
@@ -22,6 +22,28 @@ func main() {
 		fmt.Fprintf(w, "Hi, Hi")
 	})
 
+	http.HandleFunc("/checkconfig", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("activity: This is activity %s\n", r.URL.Path)
+		fmt.Printf("log: This is normal log %s\n", r.URL.Path)
+
+		// send http request to 'http://localhost:2772/'
+		// sample URL: $ curl "http://localhost:2772/applications/application_name/environments/environment_name/configurations/configuration_name"
+		applicationName := "my-appconfig-ecs-application"
+		environmentName := "Beta"
+		profileName := "my-appconfig-configuration-profile"
+		resp, err := http.Get("http://localhost:2772/applications/" + applicationName + "/environments/" + environmentName + "/configurations/" + profileName)
+		if err != nil {
+			fmt.Fprintf(w, "Error: %s", err)
+			return
+		}
+		defer resp.Body.Close()
+		respBody := make([]byte, 1024)
+		// Read all bytes from response body and convert to string
+		resp.Body.Read(respBody)
+		fmt.Fprintf(w, "Response from appconfig: %s", string(respBody))
+
+	})
+
 	fmt.Println("Start.")
 
 	log.Fatal(http.ListenAndServe(":8081", nil))
